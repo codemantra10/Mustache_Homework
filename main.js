9ed350f9f91060c4a649531acb27310a61edfbe6
codemantra10 <73803525+codemantra10@users.noreply.github.com>
2021-06-20 21:03:31 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,31 @@
+noseX="";
+noseY="";
 function preload(){
 
 }
 function setup(){
 canvas=createCanvas(400,400);
 canvas.position(550,100);
+video=createCapture(VIDEO);
+video.hide();
+console.log(ml5.version);
+posenet=ml5.poseNet(video,modelLoaded);
+posenet.on("pose",gotPoses);
 }
 function draw(){
-
+image(video,0,0,400,400);
 }
 function snapshot(){
 save("mustache_image");
+}
+function modelLoaded(){
+console.log("Model is ready and working")
+}
+function gotPoses(results){
+if (results.length>0){
+console.log(results);
+noseX=results[0].pose.nose.x;
+noseY=results[0].pose.nose.y;
+console.log(noseX+""+noseY)
+}
 }
